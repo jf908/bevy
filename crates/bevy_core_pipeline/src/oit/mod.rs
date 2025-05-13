0ece8ca41ac0365341699a88c00faf62ee0a6f83
jf908 <josh@thefindons.com>
2025-05-13 11:39:40 +0100

    Merge transparent pass

--- crates/bevy_core_pipeline/src/oit/mod.rs
+++ crates/bevy_core_pipeline/src/oit/mod.rs
@@ -134,7 +134,7 @@ impl Plugin for OrderIndependentTransparencyPlugin {
             .add_render_graph_edges(
                 Core3d,
                 (
-                    Node3d::MainTransparentPass,
+                    Node3d::MainTransmissivePass,
                     OitResolvePass,
                     Node3d::EndMainPass,
                 ),
